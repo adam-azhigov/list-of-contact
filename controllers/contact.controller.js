a52ec4cf76785a0fab4675be23507d2e6f3d63d7
azhigov <adam.azhigov97@gmail.com>
2021-09-05 23:17:27 +0300

    переименовал js в jsx

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,44 +1,42 @@
-const  Contact = require('../models/Contact.model')
+const Contact = require("../models/Contact.model");
 
 module.exports.contactController = {
-   getContacts: async (req,res) => {
-     try{
-        const contact = await Contact.find()
-       return res.json(contact)
-
-     } catch (e) {
-       console.log(e.message)
-     }
-},
-
-  createContact: async (req,res) => {
-     try {
-       const { name } = req.body;
-       const newContact  =  await Contact.create({ name})
-       return  res.status(200).json(newContact)
-     } catch (e) {
-       console.log(e.message)
-     }
+  getContacts: async (req, res) => {
+    try {
+      const contact = await Contact.find();
+      return res.status(200).json(contact);
+    } catch (e) {
+      console.log(e.message);
+    }
   },
 
-  deleteContact: async (req,res) => {
-     try{
-       const { id } = req.params
-       const contact = await Contact.findByIdAndDelete(id)
-       return  res.json("удалено")
-     }catch (e) {
-       console.log(e.message)
-     }
+  createContact: async (req, res) => {
+    try {
+      const { name } = req.body;
+      const newContact = await Contact.create({ name });
+      return res.status(200).json(newContact);
+    } catch (e) {
+      console.log(e.message);
+    }
   },
-  editContact: async (req,res) => {
-     try{
-       const { id } = req.params
-       const { name } = req.body
-       const contact = await Contact.findByIdAndUpdate(id, {name})
-       return res.json(contact)
-     }catch (e){
-       console.log(e.message)
-     }
-  }
 
-}
+  deleteContact: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const contact = await Contact.findByIdAndDelete(id);
+      return res.status(200).json("удалено");
+    } catch (e) {
+      console.log(e.message);
+    }
+  },
+  editContact: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      const contact = await Contact.findByIdAndUpdate(id, { name });
+      return res.status(200).json(contact);
+    } catch (e) {
+      console.log(e.message);
+    }
+  },
+};
